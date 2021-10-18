bd8c71d604526ae1749ca388143cfdb205e5c395
RandomUser <mad.aalachi@gmail.com>
2021-10-18 11:09:39 +0200

    Test that Backstage passes's quality adds 3 with time when 5 days or less are left . part 2

--- src/test/java/com/gildedrose/GildedRoseTest.java
+++ src/test/java/com/gildedrose/GildedRoseTest.java
@@ -135,13 +135,22 @@ class GildedRoseTest {
 
   @Test
   @DisplayName("Test that Backstage passes's quality adds 3 with time when 5 days or less are left")
-  void testBackstagePassesQuality3() {
+  void testBackstagePassesQuality31() {
     Item element = new Item("Backstage passes to a TAFKAL80ETC concert", 5, 4);
     GildedRose app = new GildedRose(new Item[] {element});
     app.updateQuality();
     assertThat(element.quality,equalTo(7));
     app.updateQuality();
     assertThat(element.quality,equalTo(10));
+  }
+
+  @Test
+  @DisplayName("Test that Backstage passes's quality adds 3 with time when 5 days or less are left part 2")
+  void testBackstagePassesQuality32() {
+    Item element = new Item("Backstage passes to a TAFKAL80ETC concert", 6, 4);
+    GildedRose app = new GildedRose(new Item[] {element});
+    app.updateQuality();
+    assertThat(element.quality,equalTo(6));
   }
 
   @Test
