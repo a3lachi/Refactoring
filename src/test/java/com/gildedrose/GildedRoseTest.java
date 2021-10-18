db76653777b3c5e3efde5900b777db572801852b
RandomUser <mad.aalachi@gmail.com>
2021-10-18 03:54:13 +0200

    Refactoring project start

---
+++ src/test/java/com/gildedrose/GildedRoseTest.java
@@ -0,0 +1,19 @@
+package com.gildedrose;
+
+import org.junit.jupiter.api.*;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.*;
+
+class GildedRoseTest {
+
+  
+
+
+
+
+
+
+
+
+
+}
