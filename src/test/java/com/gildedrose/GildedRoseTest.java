c20741c2877f13d3181d1438bd63ff56e9cc1db3
RandomUser <mad.aalachi@gmail.com>
2021-10-18 03:55:57 +0200

    Test that the name is unchanged

--- src/test/java/com/gildedrose/GildedRoseTest.java
+++ src/test/java/com/gildedrose/GildedRoseTest.java
@@ -6,7 +6,14 @@ import static org.hamcrest.Matchers.*;
 
 class GildedRoseTest {
 
-  
+  @Test
+  @DisplayName("Test that the name is unchanged")
+  void testName() {
+    Item element = new Item("foo", 0, 0);
+    GildedRose app = new GildedRose(new Item[] {element});
+    app.updateQuality();
+    assertThat(element.name, is("foo"));
+  }
 
 
 
